f2e233e928cf2a0c764e27bab7d2a63ef5f37daa
sanbo.xyz <sanbo.xyz@gmail.com>
2021-10-20 10:32:16 +0800

    add projects

---
+++ src/main/java/me/hhhaiai/testjava/actions/Main.java
@@ -0,0 +1,11 @@
+package me.hhhaiai.testjava.actions;
+
+/**
+ * @Copyright © 2021 sanbo Inc. All rights reserved.
+ * @Description: TODO
+ * @Version: 1.0
+ * @Create: 2021/10/20 10:25 上午
+ * @author: sanbo
+ */
+public class Main {
+}
